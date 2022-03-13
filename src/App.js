3e2bf9772b651025831825562841d407ccb72285
Brianrahmarela <brian.rahmarela@gmail.com>
2022-03-13 11:42:09 +0700

    fetch data user github

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 function App() {
+  const [user, setUser] = useState({
+    name: "",
+    bio: "",
+    avatar : "",
+  });
+  console.log('state user', user)
+  const [repoList, setRepoList] = useState({})
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/Brianrahmarela")
+    .then((res) => res.json())
+    .then (
+      (result) => {
+        console.log(result)
+        setUser((prev) => ({...prev, 
+          name: result.name,
+          bio: result.bio,
+          avatar: result.avatar_url,
+        }))
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  }, [])
+  
   return (
     <div className="App w-100 min-vh-100 justify-content-center align-items-center d-flex">
      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={user.avatar} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{user.name}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of
-          the card's content.
+          {user.bio}
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
